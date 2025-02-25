cc78a2c042308706e4bb85e034461327330164a2
ViViHaHaNaNa <vihaanmpatole@gmail.com>
2025-02-26 00:55:03 +0530

    Made Website layout + Matched to Figma Wireframe + Responsiveness

--- capstoneproj/src/components/Main.jsx
+++ capstoneproj/src/components/Main.jsx
@@ -1,3 +1,10 @@
+import restfood from "../assets/restauranfood.jpg"
+import bruchetta from "../assets/bruchetta.svg"
+import salad from "../assets/greek salad.jpg"
+import lemondessert from "../assets/lemon dessert.jpg"
 export default function Main() {
-    
+    return(
+        <>
+        </>
+    )
 }
